141206b62a6fe8d56cf36cb3c21e1a36bc561df5
Rikin <dolllyamar@gmail.com>
2020-12-21 04:23:23 -0500

    Added clamping to advect_2d

--- include/advect_2d.h
+++ include/advect_2d.h
@@ -1,3 +1,3 @@
 #include <Eigen/Core>
 
-void advect_2d(double dt, Eigen::MatrixXd &particles, Eigen::MatrixXd &particle_velocities);
+void advect_2d(double dt, int nx, int ny, double dx, Eigen::MatrixXd &particles, Eigen::MatrixXd &particle_velocities);
--- src/advect_2d.cpp
+++ src/advect_2d.cpp
@@ -1,5 +1,10 @@
 #include "advect_2d.h"
 
-void advect_2d(double dt, Eigen::MatrixXd &particles, Eigen::MatrixXd &particle_velocities) {
+void advect_2d(double dt, int nx, int ny, double dx, Eigen::MatrixXd &particles, Eigen::MatrixXd &particle_velocities) {
     particles += particle_velocities * dt;
+    // clamp particles that try to move out of grid
+    for (int i = 0; i < particles.rows(); i++) {
+        particles(i, 0) = fmin(nx * dx - dx / 4, fmax(dx / 4, particles(i, 0)));
+        particles(i, 1) = fmin(ny * dx - dx / 4, fmax(dx / 4, particles(i, 1)));
+    }
 }
